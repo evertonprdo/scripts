10c549a08e8fb94f6c41631f45bfbe7d900fee86
Everton C Prado Jr <ecpj.prado@gmail.com>
2025-03-24 17:56:08 -0300

    Process CSV: b'*'

--- process_csv/src/main.rs
+++ process_csv/src/main.rs
@@ -5,10 +5,6 @@ fn main() {
     process_csv(content, |x| println!("{x}"));
 }
 
-const DOUBLEQUOTE: u8 = 34;
-const LINE_FEED: u8 = 10;
-const COMMA: u8 = 44;
-
 fn process_csv<F>(csv: String, callback: F)
 where
     F: Fn(String),
@@ -20,11 +16,11 @@ where
     let mut j: usize = 0;
 
     while let Some(c) = iter.next() {
-        if *c == DOUBLEQUOTE {
+        if *c == b'"' {
             is_between_doublequote = !is_between_doublequote;
         }
 
-        if !is_between_doublequote && *c == COMMA || LINE_FEED == *c {
+        if !is_between_doublequote && *c == b',' || b'\n' == *c {
             callback(csv[j..i].to_string());
             j = i + 1;
         }
